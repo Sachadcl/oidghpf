6cab7f714406e5e31a10d76b65e08076b4a84f04
Camptbel <pacary.hassan@gmail.com>
2024-09-02 16:04:03 +0200

    userController

---
+++ src/Controller/UserController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UsersRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/user", name="user_")
+ */
+class UserController extends AbstractController
+{
+    private UsersRepository $userRepository;
+
+    public function __construct(UsersRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @Route("/{id]", name="profile")
+     */
+    public function profile(int $id): Response
+    {
+        $user = $this->userRepository->find($id);
+
+        return $this->render('user/profile.html.twig', array(
+            'user' => $user
+        ));
+    }
+
+    /**
+     * @Route("/settings", name="settings")
+     */
+    public function settings(Request $request, EntityManagerInterface $entityManager, Security $security, UserPasswordHasherInterface $userPasswordHasher): Response
+    {
+        $user = $security->getUser();
+
+        $form = $this->createForm(UsersType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            if ($user->getPlainPassword()) {
+                $user->setPassword(
+                    $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                ));
+            }
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre profil a été mis à jour avec succès.');
+            return $this->redirectToRoute('/{id}', ['id' => $user->getId()]);
+        }
+
+        return $this->render('user/settings.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
